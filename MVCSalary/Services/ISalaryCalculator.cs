f5e24e208b2a4f06ff00abed5d8c5ed224121a3f
Richard Chalk <barmybrit@gmail.com>
2023-03-27 16:00:31 +0200

    Add project files.

---
+++ MVCSalary/Services/ISalaryCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCSalary.Services
+{
+    public interface ISalaryCalculator
+    {
+        int Calculate(
+        string name, int age,
+        int totalHours);
+    }
+
+}
---
+++ MVCSalary/Services/SalaryCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace MVCSalary.Services
+{
+    public class SalaryCalculator : ISalaryCalculator
+    {
+        public int Calculate(string name, int age, int totalHours)
+        {
+            double salaryPerHour = 0;
+
+            if (age < 30)
+                salaryPerHour = 100;
+            else if (age >= 30 && age <= 50)
+                salaryPerHour = 120;
+            else
+                salaryPerHour = 130;
+
+            if (name == "Richard")
+                salaryPerHour = (int)salaryPerHour * 1.1;
+
+            return (int)salaryPerHour * totalHours;
+        }
+    }
+}
